8cd7fea3a14703dc3ccf6ffe91cacebb9a8be0fa
Graeme Smecher <gsmecher@t0.technology>
2024-08-22 12:09:41 -0700

    Update captive version of 'awaitless' extension

--- rfmux/awaitless.py
+++ rfmux/awaitless.py
@@ -8,76 +8,118 @@ import textwrap
 import IPython
 
 
+class StacktraceTidyTransformer(ast.NodeTransformer):
+    """
+    Overwrite all source-code metadata in an AST, including children.
+
+    This is a bigger version of ast.copy_location, which modifies only a single
+    node and not its children. We need it to preserve stack-trace data even
+    when we insert new nodes in an AST.
+    """
+
+    def __init__(self, source):
+        self._source = source
+
+    def visit(self, node):
+        return self.generic_visit(ast.copy_location(node, self._source))
+
+
 # Convert coroutines in assignments into tasks
 class CoroutineTransformer(ast.NodeTransformer):
-    inner_context = False
+    """
+    Convert coroutine() objects in Expr and Assignment nodes into Tasks.
+    """
 
     def visit_Module(self, node):
-        # import inspect, asyncio
+        """
+        Insert "import" statements that we'll need to use later
+        """
+
         node.body.insert(
             0,
             ast.Import(
                 names=[
-                    ast.alias(name="inspect", asname=None),
-                    ast.alias(name="asyncio", asname=None),
+                    ast.alias(name="inspect", asname="_awaitless_inspect"),
+                    ast.alias(name="asyncio", asname="_awaitless_asyncio"),
                 ]
             ),
         )
-        self.generic_visit(node)
+        return self.generic_visit(node)
+
+    def visit_FunctionDef(self, node):
+        """
+        Inhibit AST transformation when we bump into Function nodes
+
+        This inhibition is mandatory, because our AST modifications insert
+        "await" nodes that are syntactically illegal inside a synchronous
+        function context.
+        """
+
+        # subtree walk is inhibited without generic_visit(node)
         return node
 
-    def _trigger_inner_context(self, node):
-        old_inner_context = self.inner_context
-        self.inner_context = True
-        node = self.generic_visit(node)
-        self.inner_context = old_inner_context
+    def visit_AsyncFunctionDef(self, node):
+        """
+        Inhibit AST transformation inside AsyncFunctionDef nodes.
+
+        This is a judgement call - we need to draw a boundary between "I'm in an
+        interactive shell", where we do AST transformations, and "I'm in a
+        codebase" where we don't.
+
+        Because we are unable to do AST transformations on synchronous
+        functions (see visit_FunctionDef above), and because writing functions
+        seems like a "coding" and not an "interacting" activity, it seems
+        reasonable to draw the boundary at function calls of both synchronous
+        and asynchronous types.
+        """
+
         return node
 
-    # The following AST nodes trigger a shift to an "inner context", where AST
-    # rewriting is disabled.
-    visit_FunctionDef = _trigger_inner_context
-    visit_AsyncFunctionDef = _trigger_inner_context
-
-    # The following AST nodes are rewritten.
     def visit_Expr(self, node):
-        if self.inner_context:
-            return self.generic_visit(node)
+        """
+        Rewrite Expr nodes
+        """
 
         t = ast.parse(
             textwrap.dedent(
                 """
-                    _ttemp = None
-                    if inspect.iscoroutine(_ttemp):
-                        _ttemp = asyncio.create_task(_ttemp)
-                        await _ttemp
-                    _ttemp
+                    _awaitless_ttemp = None
+                    if _awaitless_inspect.iscoroutine(_awaitless_ttemp):
+                        _awaitless_ttemp = _awaitless_asyncio.create_task(_awaitless_ttemp)
+                        await _awaitless_ttemp
+                    _awaitless_ttemp
                 """
             )
         )
 
-        t.body[0].value = node.value  # assign _ttemp
+        t.body[0].value = node.value  # assign _awaitless_ttemp
 
+        # Replace node and don't generic_visit its children
+        t = StacktraceTidyTransformer(node).visit(t)
         return t.body
 
     def visit_Assign(self, node):
-        if self.inner_context:
-            return self.generic_visit(node)
+        """
+        Rewrite Assign nodes
+        """
 
         t = ast.parse(
             textwrap.dedent(
                 """
-                    _ttemp = None
-                    if inspect.iscoroutine(_ttemp):
-                        _ttemp = asyncio.create_task(_ttemp)
-                        await _ttemp
-                    target = _ttemp
+                    _awaitless_ttemp = None
+                    if _awaitless_inspect.iscoroutine(_awaitless_ttemp):
+                        _awaitless_ttemp = _awaitless_asyncio.create_task(_awaitless_ttemp)
+                        await _awaitless_ttemp
+                    target = _awaitless_ttemp
                 """
             )
         )
 
-        t.body[0].value = node.value  # assign _ttemp
+        t.body[0].value = node.value  # assign _awaitless_ttemp
         t.body[-1].targets = node.targets  # assign results
 
+        # Replace node and don't generic_visit its children
+        t = StacktraceTidyTransformer(node).visit(t)
         return t.body
 
 
@@ -88,36 +130,21 @@ def load_ipython_extension(ipython=None):
     if ipython is None:
         raise RuntimeError("Can't install awaitless: no IPython session exists!")
 
-    ipython.ast_transformers.append(CoroutineTransformer())
+    ast_transformers = ipython.ast_transformers
+    ct = CoroutineTransformer()
+
+    # If there's already an Awaitless instance installed, try to remove it -
+    # otherwise we end up multiply transforming AST nodes, which explodes
+    # very quickly
+    for t in ast_transformers:
+        if (
+            t.__class__.__module__ == ct.__class__.__module__
+            and t.__class__.__name__ == ct.__class__.__name__
+        ):
+            ast_transformers.remove(t)
+
+    ast_transformers.append(ct)
 
     # We've created an implicit eventloop dependency - ensure all cells are
     # executed in an async context
     ipython.should_run_async = lambda *a, **kw: True
-
-    # There's a bug in stack_data that's exposed by ipython "ultratb" traceback
-    # code.  Newer ipython has a workaround here:
-    #
-    #     https://github.com/ipython/ipython/pull/14286
-    #
-    # For ipython 8.20 and earlier, we monkey-patch in a backport.
-    if IPython.version_info < (8, 21, 0):
-        from IPython.core import ultratb
-
-        @property
-        def lines(self):
-            from executing.executing import NotOneValueFound
-
-            try:
-                return self._sd.lines
-            except NotOneValueFound:
-
-                class Dummy:
-                    lineno = 0
-                    is_current = False
-
-                    def render(self, *, pygmented):
-                        return "<Error retrieving source code with stack_data see ipython/ipython#13598>"
-
-                return [Dummy()]
-
-        ultratb.FrameInfo.lines = lines

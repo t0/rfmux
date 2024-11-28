67a7a685128bc2cebfc874eebe9787d173f4a547
Graeme Smecher <gsmecher@t0.technology>
2024-11-28 12:53:23 -0800

    test_spotcheck.py: check NCO frequency for consistency with DAC inverse-sinc and mix-mode settings

--- test/test_spotcheck.py
+++ test/test_spotcheck.py
@@ -28,9 +28,9 @@ import pytest
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "frequency", (100e6, 2.4e9, 2.6e9, 4.9e9, 5.1e9, 7.4e9, 7.6e9, 9.9e9)
-)
-async def test_nco_set_get_frequencies(live_session, frequency):
+    "frequency", (100e6, 2.4e9, 2.6e9, 4.9e9, 5.1e9, 7.4e9, 7.6e9, 9.9e9))
+@pytest.mark.parametrize("module", range(1, 9))
+async def test_nco_set_get_frequencies(live_session, frequency, module):
     """
     Do NCO setters and getters work correctly?
 
@@ -39,13 +39,39 @@ async def test_nco_set_get_frequencies(live_session, frequency):
     doing the right thing - just API consistency.
     """
 
+    zone = int(frequency / 2.5e9) + 1
+
     d = live_session.query(rfmux.CRS).one()
     await d.resolve()
 
-    await d.set_nco_frequency(frequency, d.UNITS.HZ, module=1)
-    assert (await d.get_nco_frequency(d.UNITS.HZ, module=1)) == pytest.approx(
+    await d.set_analog_bank(high=module > 4)
+
+    if zone <= 2:
+        # Works as expected without API warning
+        await d.set_nco_frequency(frequency, d.UNITS.HZ, module=module)
+    else:
+        # Nyquist zones 3 and 4 currently emit a bogus warning from the RFDC
+        # API
+        with pytest.warns(UserWarning):
+            await d.set_nco_frequency(frequency, d.UNITS.HZ, module=module)
+
+    assert (await d.get_nco_frequency(d.UNITS.HZ, module=module)) == pytest.approx(
         frequency, abs=0.001
     )  # expect agreement below 1 mHz
+
+    dac_filter = await d.get_dac_compensation_filter(module=module)
+
+    match(zone):
+        case 1:
+            assert dac_filter==d.DAC_COMPENSATION_FILTER.MODE1
+        case 2:
+            assert dac_filter==d.DAC_COMPENSATION_FILTER.MODE2
+        case 3:
+            assert dac_filter==d.DAC_COMPENSATION_FILTER.MODE2
+        case 4:
+            assert dac_filter==d.DAC_COMPENSATION_FILTER.MODE1
+        case _:
+            assert False
 
 
 if __name__ == "__main__":

922d01c4b735418a37cb4d167c5a9d93725a2fe7
Joshua Montgomery <joshua@t0.technology>
2024-12-27 18:11:49 -0500

    Adds in-progress R1.5 release notes notebook, as well as python implementation of the phase matrix compensation as a monkey-patched py_set_frequency

--- rfmux/algorithms/measurement/__init__.py
+++ rfmux/algorithms/measurement/__init__.py
@@ -1,1 +1,2 @@
 from . import py_get_samples
+from . import py_set_frequency
---
+++ rfmux/algorithms/measurement/py_set_frequency.py
@@ -0,0 +1,65 @@
+import pickle as pkl
+import numpy as np
+import os
+from ...core.hardware_map import macro
+from ...core.schema import CRS
+
+
+# Module-level variable to store the cable delay in nanoseconds
+CABLE_DELAY_NS = 0.0
+
+@macro(CRS, register=True)
+async def set_cable_delay(crs, length_meters, velocity_factor=0.66):
+    """
+    Set the cable delay (in nanoseconds) based on the physical length (in meters).
+    
+    By default, we use a typical velocity factor of 0.66 (66%)
+    for many coax cables. You can change 'velocity_factor' as needed.
+    
+    speed_of_light_m_per_s = 3.0e8
+    time_in_seconds = length / (speed_of_light * velocity_factor)
+    time_in_ns = time_in_seconds * 1e9
+    """
+    global CABLE_DELAY_NS  # Tell Python we're modifying the module-level variable
+    
+    speed_of_light = 3.0e8  # m/s (approximately)
+    
+    # Compute one-way delay
+    time_in_seconds = length_meters / (speed_of_light * velocity_factor)
+    
+    CABLE_DELAY_NS = time_in_seconds
+
+def get_cable_delay():
+    """
+    Retrieve the current cable delay (in nanoseconds).
+    """
+    return CABLE_DELAY_NS + 2.34e-07
+
+
+def load_pickle_from_repo():
+    rfmux_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    pkl_path = os.path.join(rfmux_dir, "core", "utils", "phase_calibrations_r1.5.pkl")
+
+    with open(pkl_path, "rb") as f:
+        data = pkl.load(f)
+    return data
+
+
+final_cals = load_pickle_from_repo()
+bin_centres = np.array([x * 625e6/512 for x in range(-256, 256)])
+
+@macro(CRS, register=True)
+async def py_set_frequency(crs : CRS, freq, channel, module):
+
+    await crs.set_frequency(freq, channel=channel, module=module)
+    
+    bin_index = np.argmin(np.abs(bin_centres - freq))-256
+    
+    latency = final_cals[channel-1][bin_index]['latency'] + get_cable_delay()
+    offset = final_cals[channel-1][bin_index]['offset']
+
+    phase_from_latency = np.pi * (1. - (2*latency) * (freq % (1./latency)))
+    delta_phase = phase_from_latency + offset
+    delta_phase = (delta_phase + np.pi) % (2*np.pi) - np.pi
+
+    await crs.set_phase(-delta_phase, crs.UNITS.RADIANS, crs.TARGET.DAC, channel=channel, module=module)

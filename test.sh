479c6cddd451ebbe7f9219d7c71183310d1890ee
Graeme Smecher <gsmecher@t0.technology>
2025-02-28 12:39:14 -0800

    regression tests: shift from plain tox to uv / tox-uv
    The uv project is much faster, and avoids the need to bootstrap a Python
    environment in order to invoke Python tooling to create another Python
    environments. (Or if it does, it's behind the scenes now).
    
    The test.sh script tells you how to install uv (rather than doing it for
    you - which is a curl | sh invocation that should upset the security
    conscious.) This will have to be folded into any github actions that
    perform testing.

--- test.sh
+++ test.sh
@@ -1,45 +1,20 @@
 #!/bin/bash
 
-# Check pre-requisites. Most of these are needed for a complete python build,
-# but it's reasonable to include other ingredients here as well.
-PACKAGES="sqlite3 readline tk libffi liblzma"
-for package in $PACKAGES;
-do
-	if ! pkg-config --exists $package; then
-		echo "Need $package to correctly build a Python environment for testing. Please install it."
-		echo "hint: sudo apt-get install libsqlite3-dev libreadline-dev tk-dev libffi-dev liblzma-dev"
-		exit -1
-	fi
-done
+if ! which -s uv
+then
+	cat <<EOF >&2
+Please install uv! Instructions are available at:
 
-# Get pyenv into our environment, installing if necessary
-if ! command -v pyenv &> /dev/null; then
+	https://docs.astral.sh/uv/getting-started/installation
 
-	if [ ! -e ~/.pyenv ]; then
-		echo "pyenv not found, installing..."
-		# This is alarming - but you're running arbitrary code
-		# regardless, whether we use git or this abomination
-		curl https://pyenv.run | bash
-	fi
+They will tell you to run something like:
 
-	# Add pyenv to PATH (consider adding this to your shell configuration)
-	export PATH="$HOME/.pyenv/bin:$PATH"
-	eval "$(pyenv init --path)"
+	curl -LsSf https://astral.sh/uv/install.sh | sh
+
+You should generally be suspicious of instructions like these.
+EOF
+	exit -1
 fi
 
-# Install necessary Python versions using pyenv
-PYTHON_VERSIONS=("3.10.13" "3.11.8" "3.12.2")
-for version in "${PYTHON_VERSIONS[@]}"; do
-    if ! pyenv versions --bare | grep -q "^$version$"; then
-        pyenv install "$version"
-    fi
-done
-
-# Optionally set a local Python version for the project
-pyenv local 3.10.13 3.11.8 3.12.2
-
-# Ensure tox is installed locally (don't use the distro version)
-pip3.10 install --upgrade pip setuptools tox
-
-# Now kick off regression tests
-python3.10 -m tox
+uv tool install -q tox --with tox-uv
+uv run tox run-parallel

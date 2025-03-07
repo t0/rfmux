7828229af2534aa7ea31183c848f9c3e5cfbf85e
Graeme Smecher <gsmecher@t0.technology>
2025-03-07 15:43:23 -0800

    QC: formatting refinements

--- test/crs_qc/test_crs_basics.py
+++ test/crs_qc/test_crs_basics.py
@@ -16,7 +16,9 @@ from .crs_qc import render_markdown, ResultTable
 @pytest.mark.qc_stage1
 @pytest.mark.asyncio
 async def test_communications(d, request, shelf, check):
-    """
+    report = [
+        render_markdown(
+            f"""
     ## Basic board communications tests
 
     Tests the following:
@@ -28,8 +30,10 @@ async def test_communications(d, request, shelf, check):
     If any of these tests fail, you should stop here and debug your test setup
     before proceeding with more complex tests.
     """
+        )
+    ]
 
-    rt = ResultTable("Check", "Status")
+    report.append(rt := ResultTable("Check", "Status"))
     hostname = f'rfmux{request.config.getoption("--serial")}.local'
 
     # 1. Can we resolve the board?
@@ -45,11 +49,10 @@ async def test_communications(d, request, shelf, check):
         # Coming up after this is a py_get_samples() call. Because
         # py_get_samples uses "module=1", we sneak in a set_analog_bank(False)
         # call here to ensure we aren't inadvertently set up for high banking.
-        text = requests.post(f"http://{hostname}/tuber", json={
-            "object":"Dfmux",
-            "method":"set_analog_bank",
-            "args":[False]
-        }).text
+        text = requests.post(
+            f"http://{hostname}/tuber",
+            json={"object": "Dfmux", "method": "set_analog_bank", "args": [False]},
+        ).text
         rt.pass_("HTTP POST", "Pass")
     except Exception as e:
         rt.fail("HTTP POST", e.message)
@@ -64,30 +67,28 @@ async def test_communications(d, request, shelf, check):
         check.fail(str(e))
 
     with shelf as x:
-        x["sections"][request.node.nodeid] = [
-            render_markdown(test_communications.__doc__),
-            rt,
-        ]
+        x["sections"][request.node.nodeid] = report
 
 
 @pytest.mark.qc_stage1
 @pytest.mark.asyncio
 async def test_metadata(d, request, shelf):
-    """
+    report = [
+        render_markdown(
+            f"""
     ## Metadata Retrieval
 
     The board includes helper functions to retrieve its own metadata. This
     function tests the structure of this metadata and also displays the results
     for record-keeping.
     """
+        )
+    ]
 
-    rt = ResultTable("Attribute", "Value")
+    report.append(rt := ResultTable("Attribute", "Value"))
     r = await d.get_firmware_release()
     for k in r:
         rt.row(k, getattr(r, k))
 
     with shelf as x:
-        x["sections"][request.node.nodeid] = [
-            render_markdown(test_metadata.__doc__),
-            rt,
-        ]
+        x["sections"][request.node.nodeid] = report

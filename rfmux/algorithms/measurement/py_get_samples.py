ae311fd74357beaa20924eb56e9faeb17c74edff
Joshua Montgomery <joshua@t0.technology>
2025-07-18 10:52:03 -0400

    crs.get_fir_stage is now called crs.get_decimation_stage()

--- rfmux/algorithms/measurement/py_get_samples.py
+++ rfmux/algorithms/measurement/py_get_samples.py
@@ -303,7 +303,7 @@ async def py_get_samples(crs: CRS,
         nperseg = num_samples // nsegments
 
         # Retrieve decimation stage => helps define final sampling freq
-        dec_stage = await crs.get_fir_stage()
+        dec_stage = await crs.get_decimation_stage(module=module)
         fs = 625e6/(256*64*(2**dec_stage))
 
         spec_data = {}

5698899ba82be6b448ff8328bf4ec800c957733e
KetanT0 <ketan.sand@t0.technology>
2025-10-29 18:01:11 -0400

    fix(get_multicast_socket): Added the line to reuse the port, this is to make sure both periscope and py get samples can bind to the same port, has to be tested on Windows

--- rfmux/streamer.py
+++ rfmux/streamer.py
@@ -440,6 +440,8 @@ def get_multicast_socket(crs_hostname):
     # Configure the socket for multicast reception
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) ### Will only work Linux > 3.9 and Mac > 10.6
+
     # Bind the socket to all interfaces on the specified port
     sock.bind(("", STREAMER_PORT))
 

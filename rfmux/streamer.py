7a8add6dded8ca9b379e70bae0a19253bd63d634
Ketant0 <ketan.sand@t0.technology>
2025-10-30 14:53:44 -0400

    Added try except for Windows. Currently noise spectrum will work only on Linux and Mac

--- rfmux/streamer.py
+++ rfmux/streamer.py
@@ -440,7 +440,10 @@ def get_multicast_socket(crs_hostname):
     # Configure the socket for multicast reception
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) ### Will only work Linux > 3.9 and Mac > 10.6
+    try:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) ### Will only work Linux > 3.9 and Mac > 10.6
+    except:
+        pass
 
     # Bind the socket to all interfaces on the specified port
     sock.bind(("", STREAMER_PORT))
